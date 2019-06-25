d0379f2f31244c6ca0ff70a1f9334219502f8964
Leo Yang <leo19941227@gmail.com>
2019-06-25 16:16:10 +0800

    setup colorsheme

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
-host=b03902130@linux1.csie.ntu.edu.tw
-scp "$host":~/.vimrc ~/
-
+cp .vimrc ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 scheme_path=~/.vim/bundle/vim-colorschemes/colors/
-scp "$host":"$scheme_path"monokai-leo.vim "$scheme_path"
+cp monokai-leo.vim "$scheme_path"
