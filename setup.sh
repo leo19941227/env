cdbdb743539d81af71cb238624203f7fad62d185
Leo Yang <leo19941227@gmail.com>
2019-06-25 16:54:29 +0800

    add tmux

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
+# SETUP TMUX
+cp .tmux.conf ~/
+
+# SETUP VIM
 cp .vimrc ~/
+
+# install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
+# install colorscheme
 scheme_path=~/.vim/bundle/vim-colorschemes/colors/
 cp monokai-leo.vim "$scheme_path"
