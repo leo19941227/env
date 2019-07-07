7ee547fa98a23486f1f622138e36cb0af5547fab
Leo Yang <leo19941227@gmail.com>
2019-07-07 22:44:13 +0800

    add alias to mount speech lab nfs

--- assets/env.sh
+++ assets/env.sh
@@ -16,5 +16,6 @@ github='https://github.com/b03902130/'
 alias home='ssh -t $linux5 "ssh leo@localhost -p 1994"'
 alias mint='ssh $mint'
 alias speech='ssh $speech'
+alias lab_nfs='sshfs -p 3122 $speech:/homes/leo1994122701'
 
 # DEFINE ENV END
