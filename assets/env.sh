acbc2f02b09c3598af1278ff8e5bcbec8b25fe68
Leo Yang <leo19941227@gmail.com>
2019-07-07 17:57:36 +0800

    new aliases

--- assets/env.sh
+++ assets/env.sh
@@ -9,10 +9,12 @@ linux3='b03902130@linux3.csie.ntu.edu.tw'
 linux4='b03902130@linux4.csie.ntu.edu.tw'
 linux5='b03902130@linux5.csie.ntu.edu.tw'
 speech='leo1994122701@sppco.ee.ntu.edu.tw'
-mint='192.168.56.101'
+mint='leo@192.168.56.101'
 github='https://github.com/b03902130/'
 
 # ALIAS
 alias home='ssh -t $linux5 "ssh leo@localhost -p 1994"'
+alias mint='ssh $mint'
+alias speech='ssh $speech'
 
 # DEFINE ENV END
