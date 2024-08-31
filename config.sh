f1b18fda71ac5717c085f8fe4176086423e45d87
Leo Yang <leo19941227@gmail.com>
2024-08-31 17:15:59 +0000

    fix config.sh

--- config.sh
+++ config.sh
@@ -23,21 +23,17 @@ done
 
 # platform-dependent settings
 if [ "$(hostname)" == "speechlab" ] && [ "$(whoami)" == "leo" ]; then
-    export work='/home/leo/d/'
-    export group='/home/leo/groups/leo1994122701/'
-    export twcc_sftp='leo19941227@xdata1.twcc.ai'
     export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
-    export PATH="/usr/local/cuda/bin:$PATH"
-    export KENLM_ROOT='/home/leo/d/tools/kenlm'
-
-    export KALDI_ROOT=/home/leo/d/tools/kaldi
-    PATH=$PATH:$KALDI_ROOT/tools/openfst
-    PATH=$PATH:$KALDI_ROOT/src/featbin
-    PATH=$PATH:$KALDI_ROOT/src/gmmbin
-    PATH=$PATH:$KALDI_ROOT/src/bin
-    PATH=$PATH:$KALDI_ROOT/src/nnetbin
-    PATH=$PATH:$KALDI_ROOT/src/ivectorbin
-    PATH=$PATH:/home/leo/d/tools/SCTK/bin
+    export KENLM_ROOT="/home/leo/d/tools/kenlm"
+    export KALDI_ROOT="/home/leo/d/tools/kaldi"
+    export PATH=$PATH:"/usr/local/cuda/bin"
+    export PATH=$PATH:$KALDI_ROOT/tools/openfst
+    export PATH=$PATH:$KALDI_ROOT/src/featbin
+    export PATH=$PATH:$KALDI_ROOT/src/gmmbin
+    export PATH=$PATH:$KALDI_ROOT/src/bin
+    export PATH=$PATH:$KALDI_ROOT/src/nnetbin
+    export PATH=$PATH:$KALDI_ROOT/src/ivectorbin
+    export PATH=$PATH:/home/leo/d/tools/SCTK/bin
 
 elif [ "$(hostname)" == "login.speech" ]; then
     export work='/groups/leo1994122701/'
@@ -46,62 +42,59 @@ elif [ "$(hostname)" == "login.speech" ]; then
 fi
 
 if cat /etc/os-release | grep -q "Amazon Linux"; then
+    alias envbmt="/apollo/bin/env -e BlueshiftModelingTools"
+    alias bufols='/apollo/env/Hoverboard-ScienceDataPipeline/bin/hadoop-prod-bufo fs -ls'
+    alias bufocat='/apollo/env/Hoverboard-ScienceDataPipeline/bin/hadoop-prod-bufo fs -cat'
+    alias mlpprep='/apollo/bin/env -e HoverboardVolumeSnapshot-MLPDataAccess prepare_datamart_data.py --content-types AUDIO -- '
+    alias mlpcat='/apollo/bin/env -e HoverboardVolumeSnapshot-MLPDataAccess get_datamart_data.py --content-type AUDIO --data-type data -- '
+    alias curlhost='curl -s https://host-discovery.hoverboard | python -m json.tool'
+    alias xsub="/apollo/bin/env -e HoverboardVolumeSnapshot-AEDevSnapshot python"
+    alias awss3="AWS_CREDENTIALS_ODIN=AWS_CREDENTIALS_ODIN s3"
+    alias hovers3cat="/apollo/env/HoverboardFileAccessPythonScripts/bin/hovers3cat -c $S3_CREDENTIAL"
+    alias hovers3put="/apollo/env/HoverboardFileAccessPythonScripts/bin/hovers3put -c $S3_CREDENTIAL"
+    alias s3putpy="/apollo/env/BlueshiftModelingTools/bin/s3put.py -c $S3_CREDENTIAL"
+    alias s3head="AWS_CREDENTIALS_ODIN=$AWS_CREDENTIALS_ODIN s3api head-object"
+    alias s3list="/apollo/env/BlueshiftModelingTools/bin/s3list.rb -c $S3_CREDENTIAL"
+    alias mlpprep='/apollo/bin/env -e HoverboardVolumeSnapshot-MLPDataAccess prepare_datamart_data.py --content-types AUDIO -- '                                                  # mlpprep $dmid
+    alias mlpstatus='/apollo/bin/env -e HoverboardVolumeSnapshot-MLPDataAccess get_job_status.py'                                                                                 # $jobid
+    alias mlpcat='/apollo/bin/env -e HoverboardVolumeSnapshot-MLPDataAccess get_datamart_data.py --content-type AUDIO --data-type data -- '                                       # mlpcat $dmid > ff.wav
+    alias mlpcatmetadata='/apollo/bin/env -e HoverboardVolumeSnapshot-MLPDataAccess get_datamart_data.py --content-type AUDIO --data-type metadata -- '                           # mlpcat $dmid > ff.wav
+    alias mlpprepSegmentedPresenceDetection='/apollo/bin/env -e HoverboardVolumeSnapshot-MLPDataAccess prepare_datamart_data.py --content-types SEGMENTED_PRESENCE_DETECTION -- ' # mlpprep $dmid alias mlpcatSegmentedPresenceDetection='/apollo/bin/env -e HoverboardVolumeSnapshot-MLPDataAccess get_datamart_data.py --content-type SEGMENTED_PRESENCE_DETECTION --data-type data -- ' #     mlpcat $dmid > ff.wav                                                                                                                                                                        alias curlhost='curl -s https://host-discovery.hoverboard | python -m json.tool'
+    alias sox='/apollo/bin/env -e AcousticModelingCore sox '
+    alias mlps3='/apollo/env/HoverboardDefaultMLPS3Tool/bin/mlps3'
+    alias hbscale='/apollo/env/HoverboardScaleCLI/bin/hover-scale scale CPU:'
+    alias scaleup='/apollo/env/HoverboardScaleCLI/bin/scale'
+    alias getip="/usr/bin/curl -s https://host-discovery.hoverboard  | jq -c '.[] | {privateIpAddress: .privateIpAddress, instanceId: .instanceId, tagName: .tags.Name, instanceType: .            instanceType, state: .state.name}'" alias sshgpu='ssh -i ~/.ssh/worker_key -o UserKnownHostsFile=~/.ssh/worker_hosts'
+    alias termgpu='/apollo/env/HoverboardScaleCLI/bin/hover-scale terminate-worker --ip'
+
+    function watch_workers() {
+        # watch_workers 3 to get workers status every 3 seconds
+        period=${1:-"1"}
+        while :; do
+            result=$(getip)
+            echo $result
+            sleep $period
+        done
+    }
+
     export s3_data_us_east_1="s3://novaboard-shared-apt-agi-3p-data-us-east-1"
     export s3_data_eu_west_1="s3://novaboard-shared-apt-agi-3p-data-mirror-eu-west-1"
+    export NUMEXPR_MAX_THREADS=32
+    export s3="/apollo/env/HoverboardAshS4/var/s3_mounts"
+
+    export CONDA_ROOT=/home/ec2-user/anaconda3
     export PYTHONPATH=/efs/ec2-user/codebase/APTA2ADataLoading/src:$PYTHONPATH
     export PYTHONPATH=/efs/ec2-user/codebase/APTUtils/src:$PYTHONPATH
     export PYTHONPATH=/efs/ec2-user/codebase/torch-fidelity:$PYTHONPATH
     export PYTHONPATH=/efs/ec2-user/codebase/audioldm_eval:$PYTHONPATH
     export PYTHONPATH=/efs/ec2-user/codebase/AudioLDM-training-finetuning:$PYTHONPATH
-
-    alias envbmt="/apollo/bin/env -e BlueshiftModelingTools"                                                                                                                                      
-    alias bufols='/apollo/env/Hoverboard-ScienceDataPipeline/bin/hadoop-prod-bufo fs -ls'                                                                                                         
-    alias bufocat='/apollo/env/Hoverboard-ScienceDataPipeline/bin/hadoop-prod-bufo fs -cat'                                                                                                       
-    alias mlpprep='/apollo/bin/env -e HoverboardVolumeSnapshot-MLPDataAccess prepare_datamart_data.py --content-types AUDIO -- '                                                                  
-    alias mlpcat='/apollo/bin/env -e HoverboardVolumeSnapshot-MLPDataAccess get_datamart_data.py --content-type AUDIO --data-type data -- '                                                       
-    alias curlhost='curl -s https://host-discovery.hoverboard | python -m json.tool'                                                                                                              
-    alias xsub="/apollo/bin/env -e HoverboardVolumeSnapshot-AEDevSnapshot python"                                                                                                                 
-    alias awss3="AWS_CREDENTIALS_ODIN=AWS_CREDENTIALS_ODIN s3"                                                                                                                                    
-    alias hovers3cat="/apollo/env/HoverboardFileAccessPythonScripts/bin/hovers3cat -c $S3_CREDENTIAL"                                                                                             
-    alias hovers3put="/apollo/env/HoverboardFileAccessPythonScripts/bin/hovers3put -c $S3_CREDENTIAL"                                                                                             
-    alias s3putpy="/apollo/env/BlueshiftModelingTools/bin/s3put.py -c $S3_CREDENTIAL"                                                                                                             
-    alias s3head="AWS_CREDENTIALS_ODIN=$AWS_CREDENTIALS_ODIN s3api head-object"                                                                                                                   
-    alias s3list="/apollo/env/BlueshiftModelingTools/bin/s3list.rb -c $S3_CREDENTIAL"                                                                                                             
-    alias mlpprep='/apollo/bin/env -e HoverboardVolumeSnapshot-MLPDataAccess prepare_datamart_data.py --content-types AUDIO -- ' # mlpprep $dmid                                                  
-    alias mlpstatus='/apollo/bin/env -e HoverboardVolumeSnapshot-MLPDataAccess get_job_status.py' # $jobid                                                                                        
-    alias mlpcat='/apollo/bin/env -e HoverboardVolumeSnapshot-MLPDataAccess get_datamart_data.py --content-type AUDIO --data-type data -- ' # mlpcat $dmid > ff.wav                               
-    alias mlpcatmetadata='/apollo/bin/env -e HoverboardVolumeSnapshot-MLPDataAccess get_datamart_data.py --content-type AUDIO --data-type metadata -- ' # mlpcat $dmid > ff.wav                   
-    alias mlpprepSegmentedPresenceDetection='/apollo/bin/env -e HoverboardVolumeSnapshot-MLPDataAccess prepare_datamart_data.py --content-types SEGMENTED_PRESENCE_DETECTION -- ' # mlpprep $dmid alias mlpcatSegmentedPresenceDetection='/apollo/bin/env -e HoverboardVolumeSnapshot-MLPDataAccess get_datamart_data.py --content-type SEGMENTED_PRESENCE_DETECTION --data-type data -- ' #     mlpcat $dmid > ff.wav                                                                                                                                                                        alias curlhost='curl -s https://host-discovery.hoverboard | python -m json.tool'                                                                                                              
-    alias sox='/apollo/bin/env -e AcousticModelingCore sox '                                                                                                                                      
-    alias mlps3='/apollo/env/HoverboardDefaultMLPS3Tool/bin/mlps3'                                                                                                                                
-    alias hbscale='/apollo/env/HoverboardScaleCLI/bin/hover-scale scale CPU:'                                                                                                                     
-    alias scaleup='/apollo/env/HoverboardScaleCLI/bin/scale'                                                                                                                                      
-    alias getip="/usr/bin/curl -s https://host-discovery.hoverboard  | jq -c '.[] | {privateIpAddress: .privateIpAddress, instanceId: .instanceId, tagName: .tags.Name, instanceType: .            instanceType, state: .state.name}'"                                                                                                                                                          alias sshgpu='ssh -i ~/.ssh/worker_key -o UserKnownHostsFile=~/.ssh/worker_hosts'                                                                                                             
-    alias termgpu='/apollo/env/HoverboardScaleCLI/bin/hover-scale terminate-worker --ip'
-
-    function watch_workers(){                                                                                                                                                                     
-        # watch_workers 3 to get workers status every 3 seconds                                                                                                                                   
-        period=${1:-"1"}                                                                                                                                                                          
-        while : ;                                                                                                                                                                                 
-        do                                                                                                                                                                                        
-            result=$(getip)                                                                                                                                                                       
-            echo $result                                                                                                                                                                          
-            sleep $period ;                                                                                                                                                                       
-        done                                                                                                                                                                                      
-        }                                                                                                                                                                                         
-                                                                                                                                                                                                
-    # variables                                                                                                                                                                                   
-    export NUMEXPR_MAX_THREADS=32                                                                                                                                                                 
-    export s3="/apollo/env/HoverboardAshS4/var/s3_mounts"
-
-    CONDA_ROOT=/home/ec2-user/anaconda3
 fi
 
 # conda env
 if [ ! -z "$CONDA_ROOT" ]; then
     # >>> conda initialize >>>
     # !! Contents within this block are managed by 'conda init' !!
-    __conda_setup="$($CONDA_ROOT'/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+    __conda_setup="$($CONDA_ROOT'/bin/conda' 'shell.bash' 'hook' 2>/dev/null)"
     if [ $? -eq 0 ]; then
         eval "$__conda_setup"
     else
