e212092f698eaf073d5535b9005faf711fbc9ff9
Shu-wen Yang <leo19941227@gmail.com>
2024-09-01 02:57:16 +0000

    finalize init_env.sh

--- utils/ssh_all_gpus.sh
+++ utils/ssh_all_gpus.sh
@@ -3,7 +3,12 @@
 ips=$(/usr/bin/curl -s https://host-discovery.hoverboard | jq -r -c '.[] | .privateIpAddress')
 for ip in $ips;
 do
-    window_name=$(echo $ip | awk -F '.' '{print $3 "." $4}')
-    tmux new-window -n ${window_name}
-    tmux send-keys 'ssh -t -o StrictHostKeyChecking=no '$ip' "bash /efs-storage/init_env.sh && tmux new -s work"' C-m
+    ping -c 4 $ip > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        window_name=$(echo $ip | awk -F '.' '{print $3 "." $4}')
+        if ! tmux list-windows | grep -q "$window_name"; then
+            tmux new-window -n ${window_name}
+            tmux send-keys 'ssh -t -o StrictHostKeyChecking=no '$ip' "source /efs-storage/init_env.sh"' C-m
+        fi
+    fi
 done
