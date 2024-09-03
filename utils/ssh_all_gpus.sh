f20dd612c84861cb684e880de920e2be5db2f1ea
Shu-wen Yang <leo19941227@gmail.com>
2024-09-03 00:59:01 +0000

    refine ssh_all_gpu script

--- utils/ssh_all_gpus.sh
+++ utils/ssh_all_gpus.sh
@@ -3,12 +3,18 @@
 ips=$(/usr/bin/curl -s https://host-discovery.hoverboard | jq -r -c '.[] | .privateIpAddress')
 for ip in $ips;
 do
-    ping -c 4 $ip > /dev/null 2>&1
+    echo "Dealing with IP ${ip}..."
+    ping -c 1 $ip > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         window_name=$(echo $ip | awk -F '.' '{print $3 "." $4}')
         if ! tmux list-windows | grep -q "$window_name"; then
+            echo "IP ${ip} does not have a window yet. Launch a window with the name ${window_name}."
             tmux new-window -n ${window_name}
             tmux send-keys 'ssh -t -o StrictHostKeyChecking=no '$ip' "source /efs-storage/env/init_hoverboard_gpu.sh"' C-m
+        else
+            echo "IP ${ip} already has a window. Skip."
         fi
+    else
+        echo "IP ${ip} is not reachable. Skip."
     fi
 done
