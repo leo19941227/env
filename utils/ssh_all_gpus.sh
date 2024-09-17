67cca22cbc3257cfee7d34091317eb094c018b6e
Shu-wen Yang <leo19941227@gmail.com>
2024-09-17 23:39:45 +0000

    refine

--- utils/ssh_all_gpus.sh
+++ utils/ssh_all_gpus.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-ips=$(getip | jq 'select(.state == "running")' | jq -r '.ip' | cut -d. -f 2,3)
+ips=$(getip | jq 'select(.state == "running")' | jq -r '.ip')
 for ip in $ips; do
     echo "Dealing with IP ${ip}..."
     if [ $? -eq 0 ]; then
-        if ! (for session in $(tmux ls | cut -d: -f 1); do tmux list-windows -t $session -F '#W' | cut -d: -f 1; done) | grep -q -E "^$ip$"; then
-            echo "IP ${ip} does not have a window yet. Launch a window with the name ${ip}."
-            tmux new-window -n ${ip}
-            tmux send-keys 'ssh -t -o StrictHostKeyChecking=no '10.0.$ip' "source /efs-storage/env/init_hoverboard_gpu.sh"' C-m
+        window_name=$(echo $ip | awk -F '.' '{print $3 "." $4}')
+        if ! (for session in $(tmux ls | cut -d: -f 1); do tmux list-windows -t $session -F '#W' | cut -d: -f 1; done) | grep -q -E "^$window_name$"; then
+            echo "IP ${ip} does not have a window yet. Launch a window with the name ${window_name}."
+            tmux new-window -n ${window_name}
+            tmux send-keys 'ssh -t -o StrictHostKeyChecking=no '$ip' "source /efs-storage/env/init_hoverboard_gpu.sh"' C-m
         else
             echo "IP ${ip} already has a window. Skip."
         fi
