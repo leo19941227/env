eae91d2123fd90b44ccadc8050bcb744aef88497
Shu-wen Yang <leo19941227@gmail.com>
2024-09-24 20:27:10 +0000

    add sample_k

---
+++ utils/sample_k.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# != "2" ]; then
+    echo "$0 <source_folder> <k> <target_folder>"
+    exit 1
+fi
+
+source_folder=$1
+k=$2
+target_folder=$3
+
+mkdir -p $target_folder
+for file in $(ls $source_folder | shuf | head -n $k);
+do
+    cp $source_folder/$file $target_folder
+done
